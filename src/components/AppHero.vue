ebe1d992d3e483978aa7c3d779bdcc1049c372b0
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-05-02 16:28:36 +0200

    Esercizio + bonus completato

---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script>
+    export default {
+        nome: "AppHero",
+        data(){
+            return{
+                
+            }
+        }
+        }
+</script>
+
+<template>
+  <div>
+    <img src="https://picsum.photos/800/1000" alt="">
+  </div>
+ 
+</template>
+
+<style scoped>
+img{
+    position: relative;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,27 @@
+<script>
+    export default {
+        nome: "AppTitle",
+        data(){
+            return{
+
+            }
+        }
+        }
+</script>
+
+<template>
+  <div>
+    <h1 class="m-0 text-white">La mia prima App con Vite</h1>
+  </div>
+ 
+</template>
+
+<style scoped>
+    h1{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 9;
+    }
+</style>
